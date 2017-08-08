f060a633e2a22d02116b3aa24fde9419345bc300
janber <472347656@qq.com>
2017-08-08 15:03:02 +0800

    annotate eslint in webpack

--- public/web/homePage/webpack.config.js
+++ public/web/homePage/webpack.config.js
@@ -3,6 +3,7 @@
  */
 var webpack = require('webpack');
 var path = require('path');
+var projectRoot = './../../../'
 module.exports = {
     entry: './main.js',
     output: {
@@ -20,6 +21,20 @@ module.exports = {
        } 
     },
     module: {
+        // preLoaders: [
+        //     {
+        //         test: /\.js$/,           // 只针对js文件
+        //         loader: 'eslint',        // 指定启用eslint-loader
+        //         // include: projectRoot,
+        //         exclude: [/node_modules/]// 剔除掉不需要利用eslint审查的文件
+        //     },
+        //     {
+        //         test: /\.vue$/,          // 只针对js文件
+        //         loader: 'eslint',        // 指定启用eslint-loader
+        //         // include: projectRoot,
+        //         exclude: [/node_modules/]// 剔除掉不需要利用eslint审查的文件
+        //     }
+        // ],
         loaders: [
             {test: /\.vue$/,loader: "vue"}, 
             {test: /\.js$/,loader: 'babel',exclude: /node_modules/},
@@ -29,5 +44,11 @@ module.exports = {
             { test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader'},
         ] 
     }, 
+    // eslint: {
+    //     configFile: path.resolve(projectRoot, '.eslintrc.js'), // 指定eslint的配置文件在哪里
+    //     failOnWarning: true, // eslint报warning了就终止webpack编译
+    //     failOnError: true, // eslint报error了就终止webpack编译
+    //     cache: true, // 开启eslint的cache，cache存在node_modules/.cache目录里
+    // },
     watch: true
 };
